3daf44c4a381935fbdda6f178777e366ebc02b40
Toshihide Hara <toshihidehr@gmail.com>
2025-03-30 02:03:12 +0900

    add node-fetch

--- src/uploader.ts
+++ src/uploader.ts
@@ -1,14 +1,22 @@
 import { createReadStream } from "node:fs";
 import path from "node:path";
+// これをコメントアウトするとnodejsのfetchが使われる
+import fetch, { FormData } from "node-fetch";
 export async function uploadFile(filePath: string, uploadUrl: string, consumeResponse: boolean) {
   const { name } = path.parse(filePath);
 
   const formData = new FormData();
 
+  const fileStream = createReadStream(filePath);
+
+  fileStream.on("close", () => {
+    console.log("fileStream closed");
+  });
+
   formData.set("file", {
     name,
     [Symbol.toStringTag]: "File",
-    stream: () => createReadStream(filePath),
+    stream: () => fileStream,
   } as unknown as Blob);
 
   try {
@@ -20,22 +28,27 @@ export async function uploadFile(filePath: string, uploadUrl: string, consumeRes
       },
     });
 
+    fileStream.destroy();
+
     console.log("Status:", response.status);
 
-    if (consumeResponse && response.body) {
-      const reader = response.body.getReader();
-      try {
-        while (true) {
-          const { done, value } = await reader.read();
-          if (done) {
-            break;
-          }
-          console.log("Chunk:", value);
-        }
-      } finally {
-        reader.releaseLock();
-      }
+    if (consumeResponse) {
+      console.log("response.body", await response.text());
     }
+    // if (consumeResponse && response.body) {
+    //   const reader = response.body.getReader();
+    //   try {
+    //     while (true) {
+    //       const { done, value } = await reader.read();
+    //       if (done) {
+    //         break;
+    //       }
+    //       console.log("Chunk:", value);
+    //     }
+    //   } finally {
+    //     reader.releaseLock();
+    //   }
+    // }
   } catch (err) {
     console.error("Upload failed:", err);
   }
